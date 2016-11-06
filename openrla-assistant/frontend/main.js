bebc63ce579f60decb3305554e16cbec4c072b36
Joe Ranweiler <josephkennethranweiler@gmail.com>
2016-11-06 09:36:08 -0800

    Remove obvious comments

--- openrla-assistant/frontend/main.js
+++ openrla-assistant/frontend/main.js
@@ -18,33 +18,18 @@ function createSplashWindow() {
 }
 
 function createWindow () {
-  // Create the browser window.
   win = new BrowserWindow({width: 800, height: 600});
-
-  // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/view/index.html`);
 
-  // Open the DevTools.
   if (devTools) {
     win.webContents.openDevTools();
   }
 
-  // Emitted when the window is closed.
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     win = null;
   });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createSplashWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', app.quit);
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
