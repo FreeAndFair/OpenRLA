1004f79ed926122004f1bb8673ece1f7e3949a73
Joe Ranweiler <josephkennethranweiler@gmail.com>
2016-11-06 09:33:55 -0800

    Quit when all windows are closed on any platform

--- openrla-assistant/frontend/main.js
+++ openrla-assistant/frontend/main.js
@@ -44,21 +44,7 @@ function createWindow () {
 app.on('ready', createSplashWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow();
-  }
-});
+app.on('window-all-closed', app.quit);
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
