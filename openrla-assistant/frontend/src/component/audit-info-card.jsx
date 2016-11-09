40433c55247e47440b3e8ccdcc687d6aa8ff4e17
Joe Ranweiler <josephkennethranweiler@gmail.com>
2016-11-08 18:49:35 -0800

    Use early return style

--- openrla-assistant/frontend/src/component/audit-info-card.jsx
+++ openrla-assistant/frontend/src/component/audit-info-card.jsx
@@ -7,14 +7,10 @@ import isAuditActive from '../selector/is-audit-active';
 
 const AuditInfoCard = ({audit, active}) => {
   if (active) {
-    return (
-      <div>Audit is ongoing.</div>
-    );
-  } else {
-    return (
-      <div>No active audit.</div>
-    );
+    return <div>Audit is ongoing.</div>;
   }
+
+  return <div>No active audit.</div>;
 };
 
 AuditInfoCard.propTypes = {
