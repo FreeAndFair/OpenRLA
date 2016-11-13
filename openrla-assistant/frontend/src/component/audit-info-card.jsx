eb11bcfaa5caf8308533ba4e43ab30aa4412653a
Joe Ranweiler <josephkennethranweiler@gmail.com>
2016-11-13 15:39:01 -0800

    Update UI of audit info card

--- openrla-assistant/frontend/src/component/audit-info-card.jsx
+++ openrla-assistant/frontend/src/component/audit-info-card.jsx
@@ -2,14 +2,18 @@ import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 
+import {
+  Card,
+  CardActions,
+  CardTitle,
+  CardText
+} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+
 import isAuditActive from '../selector/is-audit-active';
 
 
 const AuditInfoCard = ({ audit, active, navigateAudit }) => {
-  const navigateButton = (
-    <button onClick={navigateAudit}>View active audit</button>
-  );
-
   let status;
 
   if (active) {
@@ -19,10 +23,18 @@ const AuditInfoCard = ({ audit, active, navigateAudit }) => {
   }
 
   return (
-    <div>
-      {status}
-      {navigateButton}
-    </div>
+    <Card>
+      <CardTitle
+         title="Audit"
+         subtitle="Start or continue" />
+      <CardText>
+        Start or continue an audit for the current election,
+        including customization of risk-limit.
+      </CardText>
+      <CardActions>
+        <RaisedButton label="Audit" onClick={navigateAudit} />
+      </CardActions>
+    </Card>
   );
 };
 
