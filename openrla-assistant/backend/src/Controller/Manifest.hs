29b99c4031c8a91f1819caf058a64780323276ea
Joe Ranweiler <josephkennethranweiler@gmail.com>
2016-12-03 13:11:02 -0800

    Factor out manifest controller parsing, IO logic

--- openrla-assistant/backend/src/Controller/Manifest.hs
+++ openrla-assistant/backend/src/Controller/Manifest.hs
@@ -1,8 +1,8 @@
 module Controller.Manifest (create) where
 
 import           Control.Monad.IO.Class (liftIO)
-import           Data.Aeson ((.:), (.=), decode, object)
-import           Data.Aeson.Types (Object, Parser)
+import           Data.Aeson (Object, Value, (.:), (.=), decode, object)
+import           Data.Aeson.Types (Parser)
 import qualified Data.ByteString.Lazy as BSL
 import           Data.Maybe (fromJust)
 import           Data.Text (Text, unpack)
@@ -25,32 +25,36 @@ nameForManifest vendor mType mId = concat [ unpack vendor
                                           ]
 
 create :: Controller
-create state@State { .. } = parseThen p cb
+create state = parseThen createP cb
   where
-    p :: Object -> Parser (Integer, Text, Text, Text)
-    p o = do
-      electionId <- o .: "electionId"
-      vendor     <- o .: "vendor"
-      fileType   <- o .: "type"
-      srcPath    <- o .: "filePath"
-      return (electionId, vendor, fileType, srcPath)
-
-    cb (electionId, vendor, fileType, srcPath) = do
-      (newPath, newData) <- liftIO $ do
-        Q.createManifest conn (vendor, fileType, srcPath)
-        rowId <- lastInsertRowId conn
-        let manifestId = fromIntegral rowId
-        let newPath = dataRel $ nameForManifest vendor fileType manifestId
-        copyFile (unpack srcPath) newPath
-        Q.setManifestPathForId conn manifestId newPath
-        fileData <- BSL.readFile newPath
-        let mObj = fromJust (decode fileData)
-        newData <- case vendor of
-          "dominion"    -> Vendor.Dominion.processManifest state electionId fileType mObj
-          "freeandfair" -> undefined
-          _             -> error "Invalid vendor"
-        return (newPath, newData)
+    cb reqData@(_, _, fileType, _) = do
+      (newPath, newData) <- liftIO (createIO state reqData)
       json $ object [ "filePath" .= newPath
                     , "type"     .= fileType
                     , "data"     .= newData
                     ]
+
+createP :: Object -> Parser (Integer, Text, Text, Text)
+createP o = do
+  electionId <- o .: "electionId"
+  vendor     <- o .: "vendor"
+  fileType   <- o .: "type"
+  srcPath    <- o .: "filePath"
+
+  return (electionId, vendor, fileType, srcPath)
+
+createIO :: State -> (Integer, Text, Text, Text) -> IO (FilePath, Value)
+createIO state@State {..} (elId, vendor, fileType, srcPath) = do
+  Q.createManifest conn (vendor, fileType, srcPath)
+  rowId <- lastInsertRowId conn
+  let manifestId = fromIntegral rowId
+  let newPath = dataRel $ nameForManifest vendor fileType manifestId
+  copyFile (unpack srcPath) newPath
+  Q.setManifestPathForId conn manifestId newPath
+  fileData <- BSL.readFile newPath
+  let mObj = fromJust (decode fileData)
+  newData <- case vendor of
+    "dominion"    -> Vendor.Dominion.processManifest state elId fileType mObj
+    "freeandfair" -> undefined
+    _             -> error "Invalid vendor"
+  return (newPath, newData)
