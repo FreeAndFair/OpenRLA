3a58d0e5e8d97e2f4b1737addd169ec9c26520bf
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-01-25 13:46:59 -0800

    Add more election integration tests

--- openrla-assistant/backend/test/IntegrationTests.hs
+++ openrla-assistant/backend/test/IntegrationTests.hs
@@ -60,3 +60,90 @@ integrationSpec = do
     it "should set the new (and only) election as active" $ do
       post "/election" postBody
       get "/election/active" `shouldRespondWith` electionBody
+
+  around withApp $ context "Creating a second election" $ do
+    let postBodyA = [json|
+            {
+              date: "Tue Jan 01 2016 12:01:23 GMT-0000 (UTC)",
+              title: "Election A"
+            }
+          |]
+        postBodyB = [json|
+            {
+              date: "Wed Jan 02 2016 13:02:35 GMT-0000 (UTC)",
+              title: "Election B"
+            }
+          |]
+
+    it "should set the newest election as active" $ do
+      post "/election" postBodyA
+      post "/election" postBodyB
+
+      get "/election/active" `shouldRespondWith` [json|
+          {
+            date: "Wed Jan 02 2016 13:02:35 GMT-0000 (UTC)",
+            active: true,
+            id: 2,
+            title: "Election B"
+          }
+        |]
+
+    it "should return the distinct created elections" $ do
+      post "/election" postBodyA `shouldRespondWith` [json|
+          {
+            date: "Tue Jan 01 2016 12:01:23 GMT-0000 (UTC)",
+            active: true,
+            id: 1,
+            title: "Election A"
+          }
+        |]
+      post "/election" postBodyB `shouldRespondWith` [json|
+          {
+            date: "Wed Jan 02 2016 13:02:35 GMT-0000 (UTC)",
+            active: true,
+            id: 2,
+            title: "Election B"
+          }
+        |]
+
+    it "should create two elections" $ do
+      post "/election" postBodyA
+      post "/election" postBodyB
+
+      get "/election" `shouldRespondWith` [json|
+          [
+            {
+              date: "Tue Jan 01 2016 12:01:23 GMT-0000 (UTC)",
+              active: false,
+              id: 1,
+              title: "Election A"
+            },
+            {
+              date: "Wed Jan 02 2016 13:02:35 GMT-0000 (UTC)",
+              active: true,
+              id: 2,
+              title: "Election B"
+            }
+          ]
+        |]
+
+    it "should make both elections accessible via id" $ do
+      post "/election" postBodyA
+      post "/election" postBodyB
+
+      get "/election/1" `shouldRespondWith` [json|
+          {
+            date: "Tue Jan 01 2016 12:01:23 GMT-0000 (UTC)",
+            active: false,
+            id: 1,
+            title: "Election A"
+          }
+        |]
+      get "/election/2" `shouldRespondWith` [json|
+          {
+            date: "Wed Jan 02 2016 13:02:35 GMT-0000 (UTC)",
+            active: true,
+            id: 2,
+            title: "Election B"
+          }
+        |]
