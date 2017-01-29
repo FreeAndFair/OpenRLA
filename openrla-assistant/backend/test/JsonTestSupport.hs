80963cfaeed3ff43355d352890ccb2c805d4aafa
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-01-29 13:03:34 -0800

    Add `JsonTestSupport` module

---
+++ openrla-assistant/backend/test/JsonTestSupport.hs
@@ -0,0 +1,9 @@
+module JsonTestSupport where
+
+import           Data.Aeson (Value(..))
+
+
+jsonLength :: Value -> Maybe Integer
+jsonLength v = case v of
+  Array  a -> Just $ (fromIntegral . length) a
+  _        -> Nothing
