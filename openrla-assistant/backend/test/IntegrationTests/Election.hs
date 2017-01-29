f50e23022effb0e69881f4f6af5233c6070e7760
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-01-29 00:56:40 -0800

    Reuse JSON test values

--- openrla-assistant/backend/test/IntegrationTests/Election.hs
+++ openrla-assistant/backend/test/IntegrationTests/Election.hs
@@ -18,15 +18,12 @@ spec = do
           date: #{date}
         }|]
         postBodyS = A.encode postBody
-        electionBody = [json|
-            {
-              date: #{date},
-              active: true,
-              id: 1,
-              title: "POTUS 2016"
-            }
-          |]
-        electionBodyMatcher = [matchJson|#{electionBody}|]
+        electionBody = [json|{
+          date: #{date},
+          active: true,
+          id: 1,
+          title: "POTUS 2016"
+        }|]
 
     it "should create the election" $ do
       let expectedBody = [matchJson|[#{electionBody}]|]
@@ -34,15 +31,15 @@ spec = do
       get "/election" `shouldRespondWith` expectedBody
 
     it "should return the new election" $ do
-      post "/election" postBodyS `shouldRespondWith` electionBodyMatcher
+      post "/election" postBodyS `shouldRespondWithJson` electionBody
 
     it "should expose the new election via its id" $ do
       post "/election" postBodyS
-      get "/election/1" `shouldRespondWith` electionBodyMatcher
+      get "/election/1" `shouldRespondWithJson` electionBody
 
     it "should set the new (and only) election as active" $ do
       post "/election" postBodyS
-      get "/election/active" `shouldRespondWith` electionBodyMatcher
+      get "/election/active" `shouldRespondWithJson` electionBody
 
   around withApp $ context "Creating a second election" $ do
     let dateA = "Tue Jan 01 2016 12:01:23 GMT-0000 (UTC)" :: String
@@ -55,70 +52,41 @@ spec = do
           date: #{dateB},
           title: "Election B"
         }|]
+        mkElectionJsonA = \active -> [json|{
+          date: #{dateA},
+          active: #{active},
+          id: 1,
+          title: "Election A"
+        }|]
+        mkElectionJsonB = \active -> [json|{
+          date: #{dateB},
+          active: #{active},
+          id: 2,
+          title: "Election B"
+        }|]
 
     it "should set the newest election as active" $ do
       post "/election" postBodyA
       post "/election" postBodyB
 
-      get "/election/active" `shouldRespondWith` [matchJson|{
-        date: "Wed Jan 02 2016 13:02:35 GMT-0000 (UTC)",
-        active: true,
-        id: 2,
-        title: "Election B"
-      }|]
+      get "/election/active" `shouldRespondWithJson` (mkElectionJsonB True)
 
     it "should return the distinct created elections" $ do
-      post "/election" postBodyA `shouldRespondWith` [matchJson|{
-        date: #{dateA},
-        active: true,
-        id: 1,
-        title: "Election A"
-      }|]
-      post "/election" postBodyB `shouldRespondWith` [matchJson|{
-        date: #{dateB},
-        active: true,
-        id: 2,
-        title: "Election B"
-      }|]
+      post "/election" postBodyA `shouldRespondWithJson` (mkElectionJsonA True)
+      post "/election" postBodyB `shouldRespondWithJson` (mkElectionJsonB True)
 
     it "should create two elections" $ do
       post "/election" postBodyA
       post "/election" postBodyB
 
-      get "/election" `shouldRespondWith` [matchJson|
-          [
-            {
-              date: "Tue Jan 01 2016 12:01:23 GMT-0000 (UTC)",
-              active: false,
-              id: 1,
-              title: "Election A"
-            },
-            {
-              date: "Wed Jan 02 2016 13:02:35 GMT-0000 (UTC)",
-              active: true,
-              id: 2,
-              title: "Election B"
-            }
-          ]
-        |]
+      get "/election" `shouldRespondWithJson` [json|[
+        #{mkElectionJsonA False},
+        #{mkElectionJsonB True}
+      ]|]
 
     it "should make both elections accessible via id" $ do
       post "/election" postBodyA
       post "/election" postBodyB
 
-      get "/election/1" `shouldRespondWith` [matchJson|
-          {
-            date: "Tue Jan 01 2016 12:01:23 GMT-0000 (UTC)",
-            active: false,
-            id: 1,
-            title: "Election A"
-          }
-        |]
-      get "/election/2" `shouldRespondWith` [matchJson|
-          {
-            date: "Wed Jan 02 2016 13:02:35 GMT-0000 (UTC)",
-            active: true,
-            id: 2,
-            title: "Election B"
-          }
-        |]
+      get "/election/1" `shouldRespondWithJson` (mkElectionJsonA False)
+      get "/election/2" `shouldRespondWithJson` (mkElectionJsonB True)
