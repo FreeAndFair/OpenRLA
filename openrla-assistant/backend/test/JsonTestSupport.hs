bb7943d48354ec6436c21c9e1e5edb7b7fca0f3a
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-01-30 13:40:59 -0800

    Add `decodeBody` test util

--- openrla-assistant/backend/test/JsonTestSupport.hs
+++ openrla-assistant/backend/test/JsonTestSupport.hs
@@ -1,9 +1,18 @@
 module JsonTestSupport where
 
+import qualified Data.Aeson as A
 import           Data.Aeson (Value(..))
+import           Data.Maybe (fromJust)
+
+import           Network.Wai.Test (SResponse(..), simpleBody)
 
 
 jsonLength :: Value -> Maybe Integer
 jsonLength v = case v of
   Array a -> Just $ (fromIntegral . length) a
   _       -> Nothing
+
+decodeBody :: Monad m => m SResponse -> m Value
+decodeBody resp = do
+  SResponse { simpleBody } <- resp
+  return $ fromJust $ A.decode simpleBody
