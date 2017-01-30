614808475fdd5b8ef11bde8869037d25751c06e2
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-01-30 12:09:50 -0800

    Fix whitespace

--- openrla-assistant/backend/test/JsonTestSupport.hs
+++ openrla-assistant/backend/test/JsonTestSupport.hs
@@ -5,5 +5,5 @@ import           Data.Aeson (Value(..))
 
 jsonLength :: Value -> Maybe Integer
 jsonLength v = case v of
-  Array  a -> Just $ (fromIntegral . length) a
-  _        -> Nothing
+  Array a -> Just $ (fromIntegral . length) a
+  _       -> Nothing
