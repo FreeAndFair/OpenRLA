95a801b3ebf84db997c80f50323dd869090e4c28
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-01-31 00:29:26 -0800

    Start audit integration test

--- openrla-assistant/backend/test/IntegrationTests.hs
+++ openrla-assistant/backend/test/IntegrationTests.hs
@@ -2,6 +2,7 @@ module IntegrationTests where
 
 import           Test.Tasty.Hspec
 
+import qualified IntegrationTests.Audit
 import qualified IntegrationTests.Initial
 import qualified IntegrationTests.Election
 import qualified IntegrationTests.Manifest
@@ -9,6 +10,7 @@ import qualified IntegrationTests.Manifest
 
 integrationSpec :: Spec
 integrationSpec = do
+  IntegrationTests.Audit.spec
   IntegrationTests.Initial.spec
   IntegrationTests.Election.spec
   IntegrationTests.Manifest.spec
---
+++ openrla-assistant/backend/test/IntegrationTests/Audit.hs
@@ -0,0 +1,57 @@
+module IntegrationTests.Audit where
+
+import           Data.Aeson (Value(..))
+
+import           Test.Hspec.Wai
+import           Test.Tasty.Hspec
+
+import           JsonTestSupport
+import           TestSupport
+
+
+date :: String
+date = "Tue Jan 01 2016 12:01:23 GMT-0000 (UTC)"
+
+electionPostBody :: Value
+electionPostBody = [json|{
+  title: "POTUS 2016",
+  date: #{date}
+}|]
+
+auditPostBody :: Value
+auditPostBody = [json|{
+  electionId: 1,
+  date: #{date},
+  riskLimit: 0.1
+}|]
+
+auditJson :: Value
+auditJson = [json|{
+  id: 1,
+  electionId: 1,
+  date: #{date},
+  riskLimit: 0.1
+}|]
+
+spec :: Spec
+spec = do
+  around withApp $ context "Auditing" $ do
+    it "should work" $ do
+      postJson "/election" electionPostBody `shouldRespondWith` 200
+
+      auditIndexResp <- get "/audit"
+      (return auditIndexResp) `shouldRespondWith` "[]"
+
+      auditCreateResp <- postJson "/audit" auditPostBody
+
+      return auditCreateResp `shouldRespondWith` 200
+
+      createBody <- decodeBody $ return auditCreateResp
+      liftIO $ createBody `shouldBe` auditJson
+
+      auditIndexResp <- get "/audit"
+
+      return auditIndexResp `shouldRespondWith` 200
+
+      indexBody <- decodeBody $ return auditIndexResp
+      liftIO $ indexBody `shouldBe` [json|[#{auditJson}]|]
