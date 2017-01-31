d6a72cfcc736606d4e4841125a4e3d244cb2b00a
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-01-31 01:48:16 -0800

    Use `setActive`, `getById` in audit `create`

--- openrla-assistant/backend/src/OpenRLA/Statement/Audit.hs
+++ openrla-assistant/backend/src/OpenRLA/Statement/Audit.hs
@@ -1,5 +1,6 @@
 module OpenRLA.Statement.Audit where
 
+import           Data.Maybe (fromJust)
 import           Data.Text (Text)
 import qualified Database.SQLite.Simple as Sql
 import           Database.SQLite.Simple (Connection, Only(..))
@@ -17,9 +18,10 @@ create conn args = do
   let s = "insert into audit (election_id, date, risk_limit) values (?, ?, ?)"
   Sql.execute conn s args
   rowId <- Sql.lastInsertRowId conn
-  let s' = "select id, election_id, date, risk_limit from audit where id = ?"
-  rows <- Sql.query conn s' (Only rowId)
-  oneRowIO rows
+  let auId = fromIntegral rowId
+  setActive conn auId
+  audit <- getById conn auId
+  return $ fromJust audit
 
 getById :: Connection -> Integer -> IO (Maybe Audit)
 getById conn auId = Sql.query conn s (Only auId) >>= justOneIO
