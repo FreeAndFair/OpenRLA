02001eb5f2e5e124462b103bb6c6602e2d7a4303
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-01-30 23:14:18 -0800

    Accept list of file paths in ballot create endpoint

--- openrla-assistant/backend/src/OpenRLA/Controller/Ballot.hs
+++ openrla-assistant/backend/src/OpenRLA/Controller/Ballot.hs
@@ -1,7 +1,8 @@
 module OpenRLA.Controller.Ballot where
 
+import           Control.Monad (forM_)
 import           Control.Monad.IO.Class (liftIO)
-import           Data.Aeson (Object, (.:), (.:?), (.!=), object)
+import           Data.Aeson (Object, (.:))
 import           Data.Aeson.Types (Parser)
 import           Data.Maybe (maybe)
 import           Data.Text (unpack)
@@ -21,20 +22,22 @@ index State { conn } = liftIO (St.index conn (0, 20)) >>= json
 create :: Controller
 create state = parseThen createP createCb
   where
-    createCb srcPath = liftIO (createIO state srcPath) >>= json
+    createCb srcPaths = liftIO (createIO state srcPaths) >>= json
 
-createP :: Object -> Parser FilePath
+createP :: Object -> Parser [FilePath]
 createP o = do
-  srcPath <- o .: "filePath"
-  return $ unpack srcPath
+  srcPath <- o .: "filePaths"
+  return $ map unpack srcPath
 
-createIO :: State -> FilePath -> IO Ballot
-createIO State { .. } srcPath = do
-  let mkPath balId = dataDir </> "ballot" </> (show balId)
-  ballot <- St.create conn srcPath mkPath
-  let Ballot { .. } = ballot
-  copyFile srcPath balFilePath
-  return ballot
+createIO :: State -> [FilePath] -> IO [Ballot]
+createIO State { .. } srcPaths = do
+  let relPath balId = dataDir </> "ballot" </> (show balId)
+  ballots <- St.create conn srcPaths relPath
+  let copyBallot (srcPath, ballot) = do
+        let Ballot { balFilePath } = ballot
+        copyFile srcPath balFilePath
+  forM_ (zip srcPaths ballots) copyBallot
+  return ballots
 
 getById :: Controller
 getById State { conn } = do
--- openrla-assistant/backend/src/OpenRLA/Statement/Ballot.hs
+++ openrla-assistant/backend/src/OpenRLA/Statement/Ballot.hs
@@ -1,5 +1,6 @@
 module OpenRLA.Statement.Ballot where
 
+import           Control.Monad (forM)
 import qualified Database.SQLite.Simple as Sql
 import           Database.SQLite.Simple (Connection, Only(..))
 
@@ -12,17 +13,18 @@ index conn args = Sql.query conn s args
   where
     s = "select id, file_path from ballot_image limit ? offset ?"
 
-create :: Connection -> FilePath -> (Integer -> FilePath) -> IO Ballot
-create conn srcPath mkPath
+create :: Connection -> [FilePath] -> (Integer -> FilePath) -> IO [Ballot]
+create conn srcPaths relPath
   = Sql.withTransaction conn $ do
       let s = "insert into ballot_image (src_path) values ?"
-      Sql.execute conn s (Only srcPath)
-      rowId <- Sql.lastInsertRowId conn
-      let balId       = fromIntegral rowId
-          balFilePath = mkPath balId
-      let s' = "update ballot_image set file_path = ? where id = ?"
-      Sql.execute conn s' (balFilePath, balId)
-      return $ Ballot { .. }
+      forM srcPaths $ \src -> do
+        Sql.execute conn s (Only src)
+        rowId <- Sql.lastInsertRowId conn
+        let balId       = fromIntegral rowId
+            balFilePath = relPath balId
+        let s' = "update ballot_image set file_path = ? where id = ?"
+        Sql.execute conn s' (balFilePath, balId)
+        return $ Ballot { .. }
 
 getById :: Connection -> Integer -> IO (Maybe Ballot)
 getById conn balId = do
