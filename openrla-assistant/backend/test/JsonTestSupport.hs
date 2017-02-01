f9ab591d786f845ab1b6534a7a583269ae67beec
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-02-01 02:36:53 -0800

    Add `bodyShouldBe`, `getBodyId` test utils

--- openrla-assistant/backend/test/JsonTestSupport.hs
+++ openrla-assistant/backend/test/JsonTestSupport.hs
@@ -7,6 +7,8 @@ import           Data.Maybe (fromJust)
 import           Data.Text (Text)
 
 import           Network.Wai.Test (SResponse(..), simpleBody)
+import           Test.Hspec.Wai
+import           Test.Tasty.Hspec
 
 
 jsonLength :: Value -> Integer
@@ -17,3 +19,11 @@ Object o .! k = o ! k
 
 decodeBody :: SResponse -> Value
 decodeBody SResponse { simpleBody } = fromJust $ A.decode simpleBody
+
+bodyShouldBe :: SResponse -> Value -> WaiSession ()
+bodyShouldBe resp val = liftIO $ decodeBody resp `shouldBe` val
+
+getBodyId :: SResponse -> Integer
+getBodyId resp = truncate balId
+  where body = decodeBody resp
+        Number balId = body .! "id"
