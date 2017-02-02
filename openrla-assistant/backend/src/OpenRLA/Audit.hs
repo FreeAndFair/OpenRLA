988f8f63c0cf26c52c5d8349e5d90524902e66c3
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-02-02 01:30:49 -0800

    Add `computeRisk`

--- openrla-assistant/backend/src/OpenRLA/Audit.hs
+++ openrla-assistant/backend/src/OpenRLA/Audit.hs
@@ -1,1 +1,17 @@
 module OpenRLA.Audit where
+
+import           Data.Function (on)
+import           Data.List (maximumBy, partition)
+
+import           OpenRLA.Types
+
+
+computeRisk :: [ContestOutcome] -> [AuditMark] -> Double
+computeRisk outcomes marks = (2*sW)^mW * (2 - 2*sW)^mL
+  where winner = maximumBy (compare `on` coShare ) outcomes
+        winnerId = coCandidateId winner
+        sW = coShare winner
+        voteForWinner mark = amCandidateId mark == winnerId
+        (winnerVotes, loserVotes) = partition voteForWinner marks
+        mW = length winnerVotes
+        mL = length loserVotes
