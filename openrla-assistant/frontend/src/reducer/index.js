9c62d4cc0c2822c1fac77d198e31e3db6eb57d5c
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-02-03 02:01:25 -0800

    Use recursive merge in state updates

--- openrla-assistant/frontend/src/reducer/index.js
+++ openrla-assistant/frontend/src/reducer/index.js
@@ -1,14 +1,16 @@
+import { merge } from 'lodash';
+
 import defaultState from './default-state';
 
 
-const merge = (...objects) => Object.assign({}, ...objects);
+const update = (...objects) => merge({}, ...objects);
 
 
 export default (state = defaultState, action) => {
   switch (action.type) {
   case 'SET_PAGE':
     const { page } = action;
-    return merge(state, { page });
+    return update(state, { page });
   default:
     return state;
   }
