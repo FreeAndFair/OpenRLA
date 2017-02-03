ba82138611cc58cf0f560817239584526af45abd
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-02-03 03:43:01 -0800

    Update `isElectionDefined` for new default state

--- openrla-assistant/frontend/src/selector/is-election-defined.js
+++ openrla-assistant/frontend/src/selector/is-election-defined.js
@@ -1,4 +1,10 @@
 import _ from 'lodash';
 
 
-export default election => !_.isEmpty(election);
+export default election => {
+  if (_.isNil(election.id)) return false;
+  if (_.isEmpty(election.contests)) return false;
+  // TODO: check candidate existence, &c
+
+  return true;
+}
