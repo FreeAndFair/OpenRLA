d8fc3f33e67550d2d5a516f4ad37463b78745f05
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-02-04 15:45:42 -0800

    Update `ElectionSummary`, integrate w/backend
    - Make component stateful
    - Use a `DatePicker` for election date
    - Use controlled components for election info fields
    - Add add/save button for election info
    - Update backend state when clicking add/save button

--- openrla-assistant/frontend/src/component/election/election-summary.jsx
+++ openrla-assistant/frontend/src/component/election/election-summary.jsx
@@ -2,35 +2,108 @@ import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 
+import _ from 'lodash';
+
 import {
   Card,
   CardTitle,
   CardText
 } from 'material-ui/Card';
 import { List, ListItem } from 'material-ui/List';
+import DatePicker from 'material-ui/DatePicker';
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+import addElection from '../../action/addElection';
+import saveElection from '../../action/saveElection';
 
 
-const ElectionSummary = ({ election }) => {
-  return (
-    <Card>
-      <CardTitle title='Election summary' />
-      <CardText>
-        <List>
-          <ListItem
-             primaryText={election.title}
-             secondaryText='Election title' />
-          <ListItem
-             primaryText={election.date}
-             secondaryText='Election date' />
-        </List>
-      </CardText>
-    </Card>
-  );
-};
+class ElectionSummary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  addElection() {
+    this.props.addElection(this.formData());
+  }
+
+  saveElection() {
+    const { id, active } = this.props.election;
+    const { date, title } = this.formData();
+    const data = { id, title, date, active };
+
+    this.props.saveElection(data);
+  }
+
+  onDateChange(_, dateOb) {
+    const date = `${dateOb}`;
+    this.setState({ date });
+  }
+
+  onTitleChange(e) {
+    const title = e.target.value;
+    this.setState({ title });
+  }
+
+  formData() {
+    const { date, title } = this.props.election;
+    const form = { date, title };
+    Object.assign(form, this.state);
+    return form;
+  }
+
+  render() {
+    const { election, addElection, saveElection } = this.props;
+
+    const electionDefined = !_.isNil(election.id);
+
+    let button;
+    if (electionDefined) {
+      button = <RaisedButton label='Save' onClick={this.saveElection.bind(this)} />;
+    } else {
+      button = <RaisedButton label='Add' onClick={this.addElection.bind(this)} />;
+    }
+
+    const form = this.formData();
+
+    return (
+      <Card>
+        <CardTitle title='Election summary' />
+        <CardText>
+          <List>
+            <ListItem secondaryText='Election title'>
+              <TextField
+                 onChange={this.onTitleChange.bind(this)}
+                 value={form.title}
+                 id='formTitle'
+                 ref='formTitle' />
+            </ListItem>
+            <ListItem secondaryText='Election date'>
+              <DatePicker
+                 onChange={this.onDateChange.bind(this)}
+                 value={form.date && new Date(form.date)}
+                 id='formDate'
+                 ref='formDate' />
+            </ListItem>
+          </List>
+        </CardText>
+        {button}
+      </Card>
+    );
+  }
+}
 
 ElectionSummary.propTypes = {
   election: PropTypes.object.isRequired,
+  addElection: PropTypes.func.isRequired,
+  saveElection: PropTypes.func.isRequired,
 };
 
-export default ElectionSummary;
+const mapDispatchToProps = dispatch => ({
+  addElection: data => dispatch(addElection(data)),
+  saveElection: election => dispatch(saveElection(election)),
+});
+
+export default connect(null, mapDispatchToProps)(ElectionSummary);
