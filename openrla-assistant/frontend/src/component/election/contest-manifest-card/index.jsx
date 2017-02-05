a6ab9fcb5b7f9180d851422fcd122175187290a9
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-02-05 13:24:54 -0800

    Define `ContestManifestCard` as class component

--- openrla-assistant/frontend/src/component/election/contest-manifest-card/index.jsx
+++ openrla-assistant/frontend/src/component/election/contest-manifest-card/index.jsx
@@ -15,22 +15,23 @@ import ViewContestManifestButton from './view-button';
 import submitContestManifest from '../../../action/submitContestManifest';
 
 
-const ContestManifestCard = ({
-  contests,
-  uploadContestManifest,
-}) => {
-  return (
-    <Card>
-      <CardTitle
-         title='Contest Manifest'
-         subtitle='Upload or view the manifest for the contests in the election.' />
-      <CardActions>
-        <RaisedButton label="Upload" onClick={uploadContestManifest} />
-        <ViewContestManifestButton contests={contests} />
-      </CardActions>
-    </Card>
-  );
-};
+class ContestManifestCard extends React.Component {
+  render() {
+    const { contests, uploadContestManifest } = this.props;
+
+    return (
+      <Card>
+        <CardTitle
+           title='Contest Manifest'
+           subtitle='Upload or view the manifest for the contests in the election.' />
+        <CardActions>
+          <RaisedButton label="Upload" onClick={uploadContestManifest} />
+          <ViewContestManifestButton contests={contests} />
+        </CardActions>
+      </Card>
+    );
+  }
+}
 
 ContestManifestCard.propTypes = {
   contests: PropTypes.object.isRequired,
