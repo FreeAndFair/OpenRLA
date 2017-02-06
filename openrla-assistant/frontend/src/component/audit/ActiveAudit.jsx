3f011fdd046e66d98fbdbab3c66a41c29135549c
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-02-06 05:46:47 -0800

    Stub `ActiveAudit` component

---
+++ openrla-assistant/frontend/src/component/audit/ActiveAudit.jsx
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
+
+import _ from 'lodash';
+
+
+const ActiveAudit = ({ audit }) => {
+  return <div>Active audit</div>;
+};
+
+
+export default ActiveAudit;
