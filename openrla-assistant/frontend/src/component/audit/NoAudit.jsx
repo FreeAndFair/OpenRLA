ba5935ddd85e587501fa5534ed17adc68cb180d1
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-02-06 02:18:38 -0800

    Enable opening, closing audit definition dialog

--- openrla-assistant/frontend/src/component/audit/NoAudit.jsx
+++ openrla-assistant/frontend/src/component/audit/NoAudit.jsx
@@ -8,15 +8,41 @@ import {
   CardText,
   CardTitle,
 } from 'material-ui/Card';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
+
+import DefineAudit from './DefineAudit';
 
 
 class NoAudit extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = { open: false };
+
+    ['openDialog', 'closeDialog'].forEach(m => {
+      this[m] = this[m].bind(this);
+    });
   }
 
+  openDialog() {
+    this.setState({ open: true });
+  };
+
+  closeDialog() {
+    this.setState({ open: false });
+  };
+
   render() {
+    const closeButton = (
+      <FlatButton
+         label="Close"
+         primary={true}
+         onTouchTap={this.closeDialog} />
+    );
+    const actions = [closeButton];
+
     return (
       <Card>
         <CardTitle title='Audit' />
@@ -26,12 +52,21 @@ class NoAudit extends React.Component {
         <CardActions>
           <RaisedButton
              label='Start'
-             onClick={console.log} />
+             onClick={this.openDialog} />
           <RaisedButton
              disabled={true}
              label='Archive'
              onClick={console.log} />
         </CardActions>
+        <Dialog
+           title="Define new audit"
+           actions={actions}
+           modal={false}
+           open={this.state.open}
+           onRequestClose={this.closeDialog}
+           autoScrollBodyContent={true} >
+          <DefineAudit />
+        </Dialog>
       </Card>
     );
   }
