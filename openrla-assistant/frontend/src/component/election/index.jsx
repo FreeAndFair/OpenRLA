9521ce206717fcee9e0fd5887b43c76f0712831a
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-02-05 22:43:09 -0800

    Don't render ballot, CVR manifest upload cards

--- openrla-assistant/frontend/src/component/election/index.jsx
+++ openrla-assistant/frontend/src/component/election/index.jsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 
 import BallotImagesCard from './ballot-images-card';
-import BallotManifestCard from './ballot-manifest-card';
 import CandidateManifestCard from './candidate-manifest-card';
-import CVRManifestCard from './cvr-manifest-card';
 import ContestManifestCard from './contest-manifest-card';
 import ElectionSummary from './summary';
 
@@ -16,8 +14,6 @@ const Election = ({ election }) => {
       <ElectionSummary election={election} />
       <ContestManifestCard contests={election.contests} />
       <CandidateManifestCard />
-      <BallotManifestCard election={election} />
-      <CVRManifestCard election={election} />
       <BallotImagesCard />
     </div>
   );
