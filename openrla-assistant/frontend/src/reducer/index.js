60b070cf2d3f12670d2e52c57999cff367cd4931
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-02-05 21:09:17 -0800

    Remove unused actions, fix switch scoping
    - Remove unused manifest update actions
    - Begin new scope block for each case

--- openrla-assistant/frontend/src/reducer/index.js
+++ openrla-assistant/frontend/src/reducer/index.js
@@ -8,22 +8,24 @@ const update = (...objects) => merge({}, ...objects);
 
 export default (state = defaultState, action) => {
   switch (action.type) {
-  case 'SET_PAGE':
+  case 'SET_PAGE': {
     const { page } = action;
+
     return update(state, { page });
-  case 'UPDATE_ELECTION':
+  }
+  case 'UPDATE_ELECTION': {
     const { election } = action;
+
     return update(state, { election });
-  case 'UPDATE_CANDIDATE_MANIFEST':
-    return update(state, { manifest: { candidate: { uploaded: true } } });
-  case 'UPDATE_CANDIDATES':
-    return update(state, { election: { contests: action.contests }});
-  case 'UPDATE_CONTEST_MANIFEST':
-    return update(state, { manifest: { contest: { uploaded: true } } });
-  case 'UPDATE_CONTESTS':
-    const contests = action.contests;
+  }
+  case 'UPDATE_CANDIDATES': {
+    const { contests } = action;
     return update(state, { election: { contests } });
-  default:
-    return state;
+  }
+  case 'UPDATE_CONTESTS': {
+    const { contests } = action;
+    return update(state, { election: { contests } });
+  }
+  default: { return state; }
   }
 };
