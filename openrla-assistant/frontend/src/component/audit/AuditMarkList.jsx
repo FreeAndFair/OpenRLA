c97ae9f139704b3c279016c54df94c0d97905176
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-02-07 01:20:58 -0800

    Remove hardcoded synthetic stub data

--- openrla-assistant/frontend/src/component/audit/AuditMarkList.jsx
+++ openrla-assistant/frontend/src/component/audit/AuditMarkList.jsx
@@ -15,24 +15,7 @@ class AuditMarkList extends React.Component {
   }
 
   render() {
-    // temp
-    const m = (bId, cId1, cId2) => ({
-      ballotId: bId,
-      marks: {
-        1001: { contestId: 1001, candidateId: cId1 },
-        1003: { contestId: 1003, candidateId: cId2 },
-      },
-    });
-
-    // synthetic data
-    const marks = {
-      1: m(1, 1, 6),
-      2: m(2, 1, 6),
-      3: m(3, 2, 6),
-      4: m(4, 1, 6),
-      5: m(5, 3, 6),
-      6: m(6, 1, 6),
-    };
+    const { marks } = this.props;
 
     const makeMark = mark => (
       <AuditMark
