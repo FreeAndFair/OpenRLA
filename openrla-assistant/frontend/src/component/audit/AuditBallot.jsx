c4928503ea4d351128f56f8d9de4b6f960e04dfb
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-02-07 02:17:50 -0800

    Add sketch of ballot audit dialog

---
+++ openrla-assistant/frontend/src/component/audit/AuditBallot.jsx
@@ -0,0 +1,66 @@
+import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
+import { remote } from 'electron';
+
+import _ from 'lodash';
+
+import {
+  Card,
+  CardActions,
+  CardTitle,
+} from 'material-ui/Card';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+
+
+class AuditBallot extends React.Component {
+  constructor(props) {
+    super(props);
+
+    ['saveMarks'].forEach(m => {
+      this[m] = this[m].bind(this);
+    });
+  }
+
+  saveMarks() {}
+
+  render() {
+    const {
+      closeDialog,
+      dialogOpen,
+    } = this.props;
+
+    const closeButton = (
+      <FlatButton
+         label="Close"
+         primary={true}
+         onTouchTap={closeDialog} />
+    );
+    const actions = [closeButton];
+
+    return (
+        <Dialog
+          title="Audit ballot"
+           actions={actions}
+           modal={false}
+           open={dialogOpen}
+           onRequestClose={closeDialog}
+           autoScrollBodyContent={true} >
+          <RaisedButton onClick={this.saveMarks} />
+        </Dialog>
+    );
+  }
+}
+
+AuditBallot.propTypes = {
+  closeDialog: PropTypes.func.isRequired,
+  dialogOpen: PropTypes.bool.isRequired,
+};
+
+const mapDispatchToProps = (dispatch, props) => {
+  return {};
+};
+
+export default connect(null, mapDispatchToProps)(AuditBallot);
