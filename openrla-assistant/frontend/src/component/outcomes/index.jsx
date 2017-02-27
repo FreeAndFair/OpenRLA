f85884f25c6402829eac4a05943b7ce0e1694cab
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-02-26 18:09:33 -0800

    Display a placeholder message if no contest manifest uploaded

--- openrla-assistant/frontend/src/component/outcomes/index.jsx
+++ openrla-assistant/frontend/src/component/outcomes/index.jsx
@@ -14,6 +14,21 @@ import { List, ListItem } from 'material-ui/List';
 import ContestOutcome from './ContestOutcome';
 
 
+const NoContestsMessage = (
+  <Card>
+    <CardTitle title='Election outcomes' />
+    <CardText>
+      <p>
+        No contests have been defined for the current election.
+      </p>
+      <p>
+        Import contest and candidate manifests to get started!
+      </p>
+    </CardText>
+  </Card>
+);
+
+
 class Outcomes extends React.Component {
   constructor(props) {
     super(props);
@@ -28,6 +43,10 @@ class Outcomes extends React.Component {
     const contestOutcomes = _.map(contests, c => (
       <ContestOutcome key={c.id} contest={c} electionId={election.id} />
     ));
+
+    if (_.isEmpty(contestOutcomes)) {
+      return NoContestsMessage;
+    }
 
     return (
       <Card>
